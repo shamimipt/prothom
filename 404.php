56765079d540f71dbf37af04845ba7dcda9855d3
shamimipt <shamimipt@gmail.com>
2023-04-20 14:02:29 +0600

    aaded some code

--- 404.php
+++ 404.php
@@ -9,52 +9,58 @@
 
 get_header();
 ?>
+	<div class="prothom-row site-content">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-lg-12">
+				<main id="primary" class="site-main">
 
-	<main id="primary" class="site-main">
+					<section class="error-404 not-found">
+						<header class="page-header">
+							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'prothom' ); ?></h1>
+						</header><!-- .page-header -->
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'prothom' ); ?></h1>
-			</header><!-- .page-header -->
+						<div class="page-content">
+							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'prothom' ); ?></p>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'prothom' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'prothom' ); ?></h2>
-						<ul>
 							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
+							get_search_form();
+
+							the_widget( 'WP_Widget_Recent_Posts' );
 							?>
-						</ul>
-					</div><!-- .widget -->
 
-					<?php
-					/* translators: %1$s: smiley */
-					$prothom_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'prothom' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$prothom_archive_content" );
+							<div class="widget widget_categories">
+								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'prothom' ); ?></h2>
+								<ul>
+									<?php
+									wp_list_categories(
+										array(
+											'orderby'    => 'count',
+											'order'      => 'DESC',
+											'show_count' => 1,
+											'title_li'   => '',
+											'number'     => 10,
+										)
+									);
+									?>
+								</ul>
+							</div><!-- .widget -->
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+							<?php
+							/* translators: %1$s: smiley */
+							$prothom_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'prothom' ), convert_smilies( ':)' ) ) . '</p>';
+							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$prothom_archive_content" );
 
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+							the_widget( 'WP_Widget_Tag_Cloud' );
+							?>
 
-	</main><!-- #main -->
+						</div><!-- .page-content -->
+					</section><!-- .error-404 -->
+
+				</main><!-- #main -->
+			</div>
+		</div>
+	</div>
 
 <?php
 get_footer();
